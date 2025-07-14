7652049bf46bb9df57286e86cf16f73ea1e7c587
Joelcfn <elkiraque@gmail.com>
2025-07-14 12:45:43 +0100

    Update controlodoJogador.cs
    4. Make player jump if spacebar pressed

--- Assets/codigos/controlodoJogador.cs
+++ Assets/codigos/controlodoJogador.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class controlodoJogador : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+       private Rigidbody playerRb;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
     }
 }
