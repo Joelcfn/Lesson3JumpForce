68acb718de74e83932d54e585a74966f760673dd
Joelcfn <elkiraque@gmail.com>
2025-07-14 13:27:16 +0100

    8. Create a spawn manager
    8. Create a spawn manager

---
+++ Assets/codigos/spwanManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class spwanManager : MonoBehaviour
+{public GameObject obstaclePrefab;
+private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+// Start is called before the first frame update
+void Start()
+{
+    Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
